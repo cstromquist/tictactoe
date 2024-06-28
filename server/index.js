5c8dd239581cf1cbeb0691a15276809011a50363
Chris Stromquist <chriss@mac-eng09.clinicomp.com>
2024-06-28 12:39:08 -0700

    Revert "refactor: moved to game.js"
    This reverts commit dfeb694144efed8d717fc0338a37631591e03d8b.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const { Server } = require("socket.io");
 const { createServer } = require("node:http");
 const express = require("express");
-const tictactoe = require("./game");
+const tictactoe = require("./connector");
 
 const app = express();
 const server = createServer(app);
